b4301d2fed5c04c1a813f14be8e6d233e63abc62
Ramon <ramon_gs95@hotmail.com>
2020-02-08 23:01:19 +0100

    [ADD] Web App with smart contract test deployed

---
+++ src/components/App.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import Web3 from 'web3';
+
+import './App.css';
+import Main from './Main'
+import Navbar from './Navbar'
+import SocialNetwork from '../abis/SocialNetwork.json'
+
+
+class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non Ethereum browser detected. You need MetaMask.')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Loads the account
+    const accounts = await web3.eth.getAccounts()
+    // console.log(accounts)
+    this.setState({ account: accounts[0] })
+    // Network ID
+    const networkId = await web3.eth.net.getId()
+    // console.log(networkId)
+    const networkData = SocialNetwork.networks[networkId]
+    // console.log(networkData)
+    if(networkData) {
+      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address)
+      // console.log(socialNetwork)
+      this.setState({ socialNetwork })
+      const postCount = await socialNetwork.methods.postCount().call()
+      this.setState({ postCount })
+      // console.log(postCount)
+      // Loads Posts
+      for (var i = 1; i <= postCount; i++) {
+        const post = await socialNetwork.methods.posts(i).call()
+        this.setState({
+          posts: [...this.state.posts, post]
+        })
+      }
+      // console.log({ posts: this.state.posts })
+      // Sorts Posts
+      this.setState({
+        posts: this.state.posts.sort((a,b) => b.tipAmount - a.tipAmount)
+      })
+      this.setState({ loading: false })
+    } else {
+      window.alert('ReyMon PREdic network not deployed to detect Network.')
+    }
+  }
+
+  createPost(content) {
+    this.setState({ loading: true })
+    this.state.socialNetwork.methods.createPost(content).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  tipPost(id, tipAmount) {
+    this.setState({ loading: true })
+    this.state.socialNetwork.methods.tipPost(id).send({ from: this.state.account, value: tipAmount })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      socialNetwork: null,
+      postCount: 0,
+      posts: [],
+      loading: true
+    }
+    this.createPost = this.createPost.bind(this)
+    this.tipPost = this.tipPost.bind(this)
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar account={this.state.account}/>
+        { this.state.loading
+          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
+          : <Main posts={this.state.posts}
+                  createPost={this.createPost}
+                  tipPost={this.tipPost}/>
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
